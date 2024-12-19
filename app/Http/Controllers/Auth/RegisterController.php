cc86f80b7029fe327662b31c81c221bd37d789a1
leowdij08 <158173182+leowdij08@users.noreply.github.com>
2024-12-19 22:43:45 +0800

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'role' => ['required', 'in:student,lecturer,librarian,admin'],  // Validasi role
+            'role' => ['required', 'in:student,lecturer,librarian,admin,general'],  // Validasi role
         ]);
     }
 
